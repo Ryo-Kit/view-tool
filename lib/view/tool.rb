6917f4a1857bcdd3f962cc59ee5ab2e9b92fafd0
Ryo Kitagawa <ryo.kitagawa@tabula.jp>
2019-11-16 18:44:49 +0900

    initial commit

---
+++ lib/view/tool.rb
@@ -0,0 +1,9 @@
+require "view/tool/version"
+require "view/tool/renderer"
+
+module View
+  module Tool
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/view/tool/version.rb
@@ -0,0 +1,5 @@
+module View
+  module Tool
+    VERSION = "0.1.0"
+  end
+end
